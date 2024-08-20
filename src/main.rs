090f599cbc3a923fe2803190547ee2f11c58af05
roku36 <roku36000@gmail.com>
2024-08-20 14:48:11 +0900

    fix bundle of animation_button from buttonbundle to nodebundle to fit UiImage to the button

---
+++ src/main.rs
@@ -0,0 +1,217 @@
+use std::f32::consts::PI;
+use std::time::Duration;
+use bevy_panorbit_camera::*;
+use bevy::{
+    animation::{animate_targets, RepeatAnimation},
+    pbr::CascadeShadowConfigBuilder,
+    prelude::*,
+};
+
+const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+
+fn main() {
+    App::new()
+        .insert_resource(AmbientLight {
+            color: Color::WHITE,
+            brightness: 2000.,
+        })
+        .add_plugins((
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Window {
+                        fit_canvas_to_parent: true,
+                        prevent_default_event_handling: false,
+                        ..default()
+                    }
+                        .into(),
+                    ..default()
+                }),
+            PanOrbitCameraPlugin,
+        ))
+        .add_systems(Startup, setup)
+        .add_systems(Update, setup_scene_once_loaded.before(animate_targets))
+        .add_systems(Update, button_interaction_system)
+        .run();
+}
+
+#[derive(Resource)]
+struct Animations {
+    animations: Vec<AnimationNodeIndex>,
+    #[allow(dead_code)]
+    graph: Handle<AnimationGraph>,
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    mut graphs: ResMut<Assets<AnimationGraph>>,
+) {
+    // Build the animation graph
+    let mut graph = AnimationGraph::new();
+    let animations = graph
+        .add_clips(
+            [
+                GltfAssetLabel::Animation(2).from_asset("models/dog.glb"),
+                GltfAssetLabel::Animation(1).from_asset("models/dog.glb"),
+                GltfAssetLabel::Animation(0).from_asset("models/dog.glb"),
+            ]
+                .into_iter()
+                .map(|path| asset_server.load(path)),
+            1.0,
+            graph.root,
+        )
+        .collect();
+
+    // Insert a resource with the current scene information
+    let graph = graphs.add(graph);
+    commands.insert_resource(Animations {
+        animations,
+        graph: graph.clone(),
+    });
+
+    // Camera
+    commands.spawn((
+        Camera3dBundle {
+            transform: Transform::from_xyz(100.0, 100.0, 150.0)
+                .looking_at(Vec3::new(0.0, 20.0, 0.0), Vec3::Y),
+            ..default()
+        },
+        PanOrbitCamera::default(),
+    ));
+
+    // Plane
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Plane3d::default().mesh().size(500000.0, 500000.0)),
+        material: materials.add(Color::srgb(0.3, 0.5, 0.3)),
+        ..default()
+    });
+
+    // Light
+    commands.spawn(DirectionalLightBundle {
+        transform: Transform::from_rotation(Quat::from_euler(EulerRot::ZYX, 0.0, 1.0, -PI / 4.)),
+        directional_light: DirectionalLight {
+            shadows_enabled: true,
+            ..default()
+        },
+        cascade_shadow_config: CascadeShadowConfigBuilder {
+            first_cascade_far_bound: 200.0,
+            maximum_distance: 400.0,
+            ..default()
+        }
+            .into(),
+        ..default()
+    });
+
+    // Dog model
+    commands.spawn(SceneBundle {
+        scene: asset_server.load(GltfAssetLabel::Scene(0).from_asset("models/dog.glb")),
+        ..default()
+    });
+
+    // UI Buttons for animation control
+    commands
+        .spawn(NodeBundle {
+            style: Style {
+                width: Val::Percent(100.0),
+                height: Val::Percent(100.0),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            ..default()
+        })
+        .with_children(|parent| {
+            spawn_animation_button(parent, &asset_server, "images/dog_icon.png", 0);
+            spawn_animation_button(parent, &asset_server, "images/dog_icon.png", 1);
+            spawn_animation_button(parent, &asset_server, "images/dog_icon.png", 2);
+        });
+}
+
+fn spawn_animation_button(
+    parent: &mut ChildBuilder,
+    asset_server: &Res<AssetServer>,
+    image_path: &str,
+    animation_index: usize,
+) {
+    parent.spawn((
+        NodeBundle {
+            style: Style {
+                width: Val::Px(100.0),
+                height: Val::Px(100.0),
+                margin: UiRect::all(Val::Px(5.0)),
+                justify_content: JustifyContent::Center,
+                align_items: AlignItems::Center,
+                ..default()
+            },
+            background_color: NORMAL_BUTTON.into(),
+            ..default()
+        },
+        AnimationButton { animation_index },
+        UiImage::new(asset_server.load(image_path.to_string())),
+        Button,
+        Interaction::default(),
+    ));
+}
+
+#[derive(Component)]
+struct AnimationButton {
+    animation_index: usize,
+}
+
+fn setup_scene_once_loaded(
+    mut commands: Commands,
+    animations: Res<Animations>,
+    mut players: Query<(Entity, &mut AnimationPlayer), Added<AnimationPlayer>>,
+    mut interaction_query: Query<(&Interaction, &AnimationButton), (Changed<Interaction>, With<Button>)>,
+) {
+    for (entity, mut player) in &mut players {
+        let mut transitions = AnimationTransitions::new();
+
+        transitions
+            .play(&mut player, animations.animations[0], Duration::ZERO)
+            .repeat();
+
+        commands
+            .entity(entity)
+            .insert(animations.graph.clone())
+            .insert(transitions);
+    }
+
+    // Process button presses to change animations
+    for (interaction, animation_button) in &mut interaction_query {
+        if *interaction == Interaction::Pressed {
+            for (_, mut player) in &mut players {
+                let mut transitions = AnimationTransitions::new();
+                transitions
+                    .play(&mut player, animations.animations[animation_button.animation_index], Duration::from_millis(250))
+                    .repeat();
+            }
+        }
+    }
+}
+
+
+fn button_interaction_system(
+    mut interaction_query: Query<
+        (&Interaction, &AnimationButton),
+        (Changed<Interaction>, With<Button>),
+    >,
+    animations: Res<Animations>,
+    mut animation_players: Query<(&mut AnimationPlayer, &mut AnimationTransitions)>,
+) {
+    for (interaction, animation_button) in &mut interaction_query {
+        if *interaction == Interaction::Pressed {
+            for (mut player, mut transitions) in &mut animation_players {
+                transitions
+                    .play(
+                        &mut player,
+                        animations.animations[animation_button.animation_index],
+                        Duration::from_millis(250),
+                    )
+                    .repeat();
+            }
+        }
+    }
+}
